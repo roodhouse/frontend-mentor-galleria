00785dc7456e4c04ab02e0e4925600a42430c830
roodhouse <rughjm@gmail.com>
2023-08-02 16:36:12 -0500

    lightbox dt done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header'
 import Gallery from './components/Gallery'
 
 // dtView
+  // check all profiles and make sure they are good
 // go through and redo the grid for each view
 // set max width and flex to keep it center
 
