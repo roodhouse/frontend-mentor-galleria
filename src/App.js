b84e07e515e870538809e69c7a4962f5df8114cb
roodhouse <rughjm@gmail.com>
2023-08-02 18:53:11 -0500

    fixes in

--- src/App.js
+++ src/App.js
@@ -2,20 +2,15 @@ import './App.css';
 import Header from './components/Header'
 import Gallery from './components/Gallery'
 
-// dtView
-  // close icon is off as screen increases, use % instead?
-// go through and redo the grid for each view
-// set max width and flex to keep it center
-
 function App() {
   
   return (
-    <div id='mainWrapper'>
-      <div id='mainContainer' className='pt-6 md:pt-[29px] xl:pt-10'>
+    <div id='mainWrapper' className='flex justify-center'>
+      <div id='mainContainer' className='pt-6 md:pt-[29px] xl:pt-10 max-w-[375px] md:max-w-[768px] lg:max-w-[1024px]  xl:max-w-[1280px] lg:w-full'>
         <div id='headerWrapper' className='border-b border-b-medGray px-6 pb-6 md:px-10 md:pb-[27px] xl:px-0 xl:mx-10 xl:pb-10'>
           <Header />
         </div>
-        <div id='galleryWrapper' className='pt-6 md:pt-10'>
+        <div id='galleryWrapper' className='pt-6 md:pt-10 lg:pt-0'>
           <Gallery />
         </div>
       </div>
