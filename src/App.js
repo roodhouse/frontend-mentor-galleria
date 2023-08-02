ab10030beed23c329abec51964b51828da48892d
roodhouse <rughjm@gmail.com>
2023-08-02 12:52:46 -0500

    dt header done

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
   
   return (
     <div id='mainWrapper'>
-      <div id='mainContainer' className='pt-6 md:pt-[29px]'>
-        <div id='headerWrapper' className='border-b border-b-medGray px-6 pb-6 md:px-10 md:pb-[27px]'>
+      <div id='mainContainer' className='pt-6 md:pt-[29px] xl:pt-10'>
+        <div id='headerWrapper' className='border-b border-b-medGray px-6 pb-6 md:px-10 md:pb-[27px] xl:px-0 xl:mx-10 xl:pb-10'>
           <Header />
         </div>
         <div id='galleryWrapper' className='pt-6 md:pt-10'>
